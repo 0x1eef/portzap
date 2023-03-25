88b540da9a14581b6e91ff2358716917506483f1
0x1eef <0x1eef@protonmail.com>
2023-03-25 02:53:15 -0300

    Separate commands into libexec/portzap/commands/

---
+++ libexec/portzap/functions/requirements.sh
@@ -0,0 +1,26 @@
+require_deps() {
+    deps=$1
+    for dep in $deps; do
+        which -s $dep
+        if [ $? -ne 0 ]; then
+            echo $dep is required, but not found
+            exit 1
+        fi
+    done
+}
+
+require_group() {
+    group=$1
+    cmd=$(id -Gn | tr ' ' '\n' | grep ^${group}$)
+    if [ $cmd != $group ]; then
+        echo "You must be a member of the '$group' group to run this command."
+        exit 1
+    fi
+}
+
+require_root() {
+    if [ $(id -u) -ne 0 ]; then
+        echo "The install command must be run as root."
+        exit 1
+    fi
+}
