afac519dc012b263440c1718978db82d102ff500
0x1eef <0x1eef@protonmail.com>
2026-02-01 22:08:34 -0300

    Update mdo.sh

--- libexec/portzap/functions/mdo.sh
+++ libexec/portzap/functions/mdo.sh
@@ -6,6 +6,29 @@ get_system_rules()
     sysctl -n security.mac.do.rules 2> /dev/null || true
 }
 
+get_portzap_rules()
+{
+    local libexec
+    libexec=$1
+    "${libexec}"/scripts/get-mdo-rule | tr ';' '\n' | sed '/^$/d'
+}
+
+is_rule_enabled()
+{
+    local system_rules rule_list syslist entry
+    system_rules=$1
+    rule_list=$2
+
+    syslist=$(printf "%s" "${system_rules}" | tr ';' '\n')
+    while IFS= read -r entry; do
+        [ -z "${entry}" ] && continue
+        printf "%s\n" "${syslist}" | grep -Fxq -- "${entry}" || return 1
+    done <<EOFRULES
+${rule_list}
+EOFRULES
+    return 0
+}
+
 merge_rules()
 {
     local lh
@@ -38,27 +61,4 @@ join_rules()
     local rule_list
     rule_list=$1
     printf "%s" "${rule_list}" | paste -sd ';' -
-}
-
-get_portzap_rules()
-{
-    local libexec
-    libexec=$1
-    "${libexec}"/scripts/get-mdo-rule | tr ';' '\n' | sed '/^$/d'
-}
-
-is_rule_enabled()
-{
-    local system_rules rule_list syslist entry
-    system_rules=$1
-    rule_list=$2
-
-    syslist=$(printf "%s" "${system_rules}" | tr ';' '\n')
-    while IFS= read -r entry; do
-        [ -z "${entry}" ] && continue
-        printf "%s\n" "${syslist}" | grep -Fxq -- "${entry}" || return 1
-    done <<EOFRULES
-${rule_list}
-EOFRULES
-    return 0
 }
