7f0d1d4b9627a58eb1b1be403054fdb08e4705eb
0x1eef <0x1eef@protonmail.com>
2026-02-01 21:36:30 -0300

    Add functions/mdo.sh

---
+++ libexec/portzap/functions/mdo.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+get_system_rules()
+{
+    sysctl -n security.mac.do.rules 2> /dev/null || true
+}
+
+get_portzap_rules()
+{
+    libexec=$1
+    "${libexec}"/scripts/get-mdo-rule | tr ';' '\n' | sed '/^$/d'
+}
+
+is_rule_enabled()
+{
+    system_rules=$1
+    rule_list=$2
+
+    syslist=$(printf "%s" "${system_rules}" | tr ';' '\n')
+    while IFS= read -r entry; do
+        [ -z "${entry}" ] && continue
+        printf "%s\n" "${syslist}" | grep -Fxq -- "${entry}" || return 1
+    done <<EOFRULES
+${rule_list}
+EOFRULES
+    return 0
+}
